b00d0a58829b5d9caa6b239adc46964777c0fc84
sl20016-prn335-cicloII-2022 <101028905+Andre0100@users.noreply.github.com>
2022-11-25 17:33:21 -0600

    Add files via upload

---
+++ guia07/src/main/java/ues/occ/edu/sv/inenieria/boundary/BackingBean.java
@@ -0,0 +1,14 @@
+package ues.occ.edu.sv.inenieria.boundary;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author andre
+ */
+public class BackingBean {
+    
+}
